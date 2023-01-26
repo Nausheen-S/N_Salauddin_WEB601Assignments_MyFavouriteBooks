3dde84f2fe17702659f6b44b4dbe571177c8bc68
NausheenSalauddin <w0805026@myscc.ca>
2023-01-26 13:22:10 -0500

    completed component

--- N_Salauddin_MyFavouriteBooks/src/app/content-card/content-card.component.ts
+++ N_Salauddin_MyFavouriteBooks/src/app/content-card/content-card.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { ContentList } from '../helper-files/content-list';
+import {Content} from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
   styleUrls: ['./content-card.component.scss']
 })
-export class ContentCardComponent {
+export class ContentCardComponent  {
+  contentList: ContentList;
+
+  constructor() {
+    this.contentList = new ContentList();
+
+    this.contentList.add({
+      id: 1,
+      title: "Content 1",
+      description: "This is my content 1",
+      creator: "John Doe",
+      imgURL: "../../assets/images/WebDevLogo.png",
+      type: "blog 1"
+    });
+
+    this.contentList.add({
+      id: 2,
+      title: "Content 2",
+      description: "This is my content 2",
+      creator: "John Doe",
+      imgURL: "../../assets/images/WebDevLogo.png",
+      type: "blog 2"
+    });
+
+    this.contentList.add({
+      id: 3,
+      title: "Content 3",
+      description: "This is my content 3",
+      creator: "John Doe",
+      imgURL: "../../assets/images/WebDevLogo.png",
+      type: "blog 3"
+    });
+  }
 
 }
