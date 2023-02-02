a026abb389365f406646af041708786845f47414
NausheenSalauddin <w0805026@myscc.ca>
2023-02-02 15:31:23 -0500

    completed contentlist component
    displayed results and added to main page

--- N_Salauddin_MyFavouriteBooks/src/app/content-list/content-list.component.ts
+++ N_Salauddin_MyFavouriteBooks/src/app/content-list/content-list.component.ts
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-list.component.scss']
 })
 export class ContentListComponent {
-
+  contentItems = [
+    {
+      id: 1,
+      image: "../../assets/images/WebDevLogo.png",
+      title: "book 1",
+      description: "Description of book 1",
+      creator: "Creator 1",
+      type: "Text",
+      tags: ["tag1", "tag2"]
+    },
+    {
+      id: 2,
+      image: "../../assets/images/WebDevLogo.png",
+      title: "book 2",
+      description: "Description of book 2",
+      creator: "Creator 2",
+      type: "Image",
+      tags: ["tag3", "tag4"]
+    },
+    {
+      id: 3,
+      image: "../../assets/images/WebDevLogo.png",
+      title: "book 3",
+      description: "Description of book 3",
+      creator: "Creator 3",
+      type: "Video",
+      tags: ["tag5", "tag6"]
+    },
+    {
+      id: 4,
+      image: "../../assets/images/WebDevLogo.png",
+      title: "book 4",
+      description: "Description of book 4",
+      creator: "Creator 4",
+      type: "Text",
+      tags: ["tag7", "tag8"]
+    },
+    {
+      id: 5,
+      image: "../../assets/images/WebDevLogo.png",
+      title: "book 5",
+      description: "Description of book 5",
+      creator: "Creator 5",
+      type: "Image",
+      tags: ["tag9", "tag10"]
+    },
+    {
+      id: 6,
+      image: "../../assets/images/WebDevLogo.png",
+      title: "book 6",
+      description: "Description of book 6",
+      creator: "Creator 6",
+      type: "Video",
+      tags: ["tag11", "tag12"]
+    }
+  ];
 }
