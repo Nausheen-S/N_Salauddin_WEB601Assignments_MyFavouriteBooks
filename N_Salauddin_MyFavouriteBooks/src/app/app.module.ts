b4f4c582fbb5e293f3a4d9d8ace12e24187997df
NausheenSalauddin <w0805026@myscc.ca>
2023-02-21 10:08:26 -0500

    added bold and underline styles

--- N_Salauddin_MyFavouriteBooks/src/app/app.module.ts
+++ N_Salauddin_MyFavouriteBooks/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { FilterContentPipe } from './filter-content.pipe';
+import { HoverAffectDirective } from './hover-affect.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentCardComponent,
     ContentListComponent,
-    FilterContentPipe
+    FilterContentPipe,
+    HoverAffectDirective
   ],
   imports: [
     BrowserModule,
