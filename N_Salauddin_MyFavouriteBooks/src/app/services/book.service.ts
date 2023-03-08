083f7814f4e887325cbb464d0e162acd31696fc3
NausheenSalauddin <w0805026@myscc.ca>
2023-03-08 14:55:06 -0500

    created service

---
+++ N_Salauddin_MyFavouriteBooks/src/app/services/book.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Content } from '../helper-files/content-interface';
+import { CONTENT } from '../helper-files/contentDb';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  constructor() { }
+  getContent(): Observable<Content[]> {
+    return of(CONTENT);
+  }
+
+  getContentById(id: number): Observable<Content | undefined> {
+    return of(CONTENT.find(item => item.id === id));
+  }
+}
