00fb0126dc6bcd091811b1e505f2c71e40f3dc68
NausheenSalauddin <w0805026@myscc.ca>
2023-03-08 14:55:06 -0500

    created service

---
+++ N_Salauddin_MyFavouriteBooks/src/app/helper-files/contentDb.ts
@@ -0,0 +1,64 @@
+import { Content } from './content-interface';
+export const CONTENT: Content[] = [
+    {
+        id: 1,
+        image: "",
+        title: "book 1",
+        description: "Description of book 1",
+        creator: "Creator 1",
+        type: "HardBound",
+        tags: ["tag1", "tag2"]
+    },
+    {
+        id: 2,
+        image: "../../assets/images/WebDevLogo.png",
+        title: "book 2",
+        description: "Description of book 2",
+        creator: "Creator 2",
+        type: "SoftBound",
+        tags: ["tag3", "tag4"]
+    },
+    {
+        id: 3,
+        image: "../../assets/images/WebDevLogo.png",
+        title: "book 3",
+        description: "Description of book 3",
+        creator: "Creator 3",
+        type: "HardBound",
+        tags: ["tag5", "tag6"]
+    },
+    {
+        id: 4,
+        image: "../../assets/images/WebDevLogo.png",
+        title: "book 4",
+        description: "Description of book 4",
+        creator: "Creator 4",
+        type: "HardBound",
+        tags: ["tag7", "tag8"]
+    },
+    {
+        id: 5,
+        image: "../../assets/images/WebDevLogo.png",
+        title: "book 5",
+        description: "Description of book 5",
+        creator: "Creator 5",
+        type: "SoftBound",
+        tags: ["tag9", "tag10"]
+    },
+    {
+        id: 6,
+        image: "../../assets/images/WebDevLogo.png",
+        title: "book 6",
+        description: "Description of book 6",
+        creator: "Creator 6",
+        tags: ["tag11", "tag12"]
+    },
+    {
+        id: 7,
+        image: "",
+        title: "book 7",
+        description: "Description of book 7",
+        creator: "Creator 7",
+        tags: ["tag11", "tag12"]
+    }
+];
