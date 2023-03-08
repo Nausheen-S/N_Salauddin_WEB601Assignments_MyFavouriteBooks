5cc7ad1bd05c3ebbe43352d1bdfef3224f7f30f6
NausheenSalauddin <w0805026@myscc.ca>
2023-03-08 18:31:50 -0500

    added app message

--- N_Salauddin_MyFavouriteBooks/src/app/app.module.ts
+++ N_Salauddin_MyFavouriteBooks/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { FilterContentPipe } from './filter-content.pipe';
 import { HoverAffectDirective } from './hover-affect.directive';
+import { AppMessagesComponent } from './app-messages/app-messages.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HoverAffectDirective } from './hover-affect.directive';
     ContentCardComponent,
     ContentListComponent,
     FilterContentPipe,
-    HoverAffectDirective
+    HoverAffectDirective,
+    AppMessagesComponent
   ],
   imports: [
     BrowserModule,
