1cc43209b0c3f1e83f239c041813100492e9a322
NausheenSalauddin <w0805026@myscc.ca>
2023-03-11 12:23:31 -0500

    added imports and modified id

--- N_Salauddin_MyFavouriteBooks/src/app/services/in-memory-data.service.ts
+++ N_Salauddin_MyFavouriteBooks/src/app/services/in-memory-data.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Content } from '../helper-files/content-interface';
+import { CONTENT } from '../helper-files/contentDb';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryDataService {
 
-  constructor() { }
+export class InMemoryDataService implements InMemoryDbService {
+
+  createDb() {
+    const content: Content[] = CONTENT;
+    return { content };
+  }
+
+  genId(content: Content[]): number {
+    return content.length > 0 ? Math.max(...content.map(c => c.id || 0)) + 1 : 2000;
+  }
 }
