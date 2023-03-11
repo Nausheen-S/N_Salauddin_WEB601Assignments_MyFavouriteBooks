70de285700314210ab6e717182dbd7a9080649e7
NausheenSalauddin <w0805026@myscc.ca>
2023-03-11 12:23:31 -0500

    added imports and modified id

--- N_Salauddin_MyFavouriteBooks/src/app/app.module.ts
+++ N_Salauddin_MyFavouriteBooks/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
@@ -20,7 +24,12 @@ import { AppMessagesComponent } from './app-messages/app-messages.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+      delay: 1000,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
